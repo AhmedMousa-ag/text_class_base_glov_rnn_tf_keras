e64a0b41418b2edd379bd8ec6534224502dae824
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-12-01 19:13:43 +0200

    debugged

--- download_embedding.sh
+++ download_embedding.sh
@@ -3,14 +3,14 @@ export embed_file_name=glove.6B."$embed_dim"d.txt
 # For refrencess please visit https://nlp.stanford.edu/projects/glove/ 
 # There are more trained embeddings that were trained on more corpus, check prev site for more.
 
-wget http://nlp.stanford.edu/data/glove.6B.zip /temp/
+wget -P /tmp http://nlp.stanford.edu/data/glove.6B.zip
 
-unzip -q /temp/glove.6B.zip
+unzip -q /tmp/glove.6B.zip
 
-cp /temp/"$embed_file_name" /opt/app/model/artifacts/trained_embed/
+cp /tmp/"$embed_file_name" /opt/app/model/artifacts/trained_embed/
 
 
 
-echo 'export embed_dim=$embed_dim' > ~/.profile
-echo 'export embed_file_name=$embed_file_name' > ~/.profile
+echo 'export embed_dim=$embed_dim' >> ~/.profile
+echo 'export embed_file_name=$embed_file_name' >> ~/.profile
 
