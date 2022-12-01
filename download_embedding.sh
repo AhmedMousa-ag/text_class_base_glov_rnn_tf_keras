44dcd595d0aac5fadd3748bcdcfd770f68ad3396
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-12-01 19:20:38 +0200

    added comments

--- download_embedding.sh
+++ download_embedding.sh
@@ -1,5 +1,5 @@
 export embed_dim=300 #Must be declared. #available dims are 50, 100, 200 and 500 
-export embed_file_name=glove.6B."$embed_dim"d.txt
+export embed_file_name=glove.6B."$embed_dim"d.txt #Must be decleared as it will be used during training
 # For refrencess please visit https://nlp.stanford.edu/projects/glove/ 
 # There are more trained embeddings that were trained on more corpus, check prev site for more.
 
@@ -11,6 +11,6 @@ cp /tmp/"$embed_file_name" /opt/app/model/artifacts/trained_embed/
 
 
 
-echo 'export embed_dim=$embed_dim' >> ~/.profile
-echo 'export embed_file_name=$embed_file_name' >> ~/.profile
+echo 'export embed_dim=$embed_dim' >> ~/.profile #To keep env variable on the system after restarting
+echo 'export embed_file_name=$embed_file_name' >> ~/.profile #To keep env variable on the system after restarting
 
