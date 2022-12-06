c8f9478dee55978a728aad34ed4c58809f346b5c
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-12-06 21:56:03 +0200

    debugged

--- app/config.py
+++ app/config.py
@@ -55,7 +55,7 @@ check_dir(MODEL_SAVE_PATH)
 
 EMBED_DIM = os.getenv("embed_dim")
 embed_file_name = os.getenv("embed_file_name")
-TRAINED_EMBEDD_PATH = os.path.join(MODEL_SAVE_PATH,"trained_embed",embed_file_name)
+TRAINED_EMBEDD_PATH = os.path.join(MODEL_SAVE_PATH,"trained_embed")
 check_dir(TRAINED_EMBEDD_PATH)
 
 # os.path.join("Utils","preprocess","artifacts")
